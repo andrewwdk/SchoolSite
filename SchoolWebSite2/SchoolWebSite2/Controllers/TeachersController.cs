21abb97ab36019cf608913c4913ccfe69f63e67b
Andrei Zhydzenka <zhidenko_1999@mail.ru>
2020-04-06 01:06:39 +0300

    Add list of teachers.

---
+++ SchoolWebSite2/SchoolWebSite2/Controllers/TeachersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SchoolWebSite2.Models;
+
+namespace SchoolWebSite2.Controllers
+{
+    public class TeachersController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new SchoolWebsiteDatabaseEntities();
+            var teachers = db.Person.Where(p => p.Role == "Учитель");
+            return View(teachers);
+        }
+    }
+}
